e9eaceed4b8ea3b9ab6d28c07fe9b8ec96c42d5b
leh <leticiazanellattodeoliveira@gmail.com>
2025-06-13 21:34:54 -0300

    modelagem do gr-16

---
+++ gr-16/gr-16/tets.py
@@ -0,0 +1,8 @@
+import os
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [10, 20, 25, 30])
+plt.title('Exemplo de gráfico simples')
+plt.show()
+
+#print(os.path.exists('C:/Users/Leticia/Desktop/Facul/IC/GRS/GR-16 - Qtde acadêmicos matriculados (em andamento e concluída) por Campus, Curso Geral, Curso, Série, Faixa Etária e Sexo.xlsx'))
